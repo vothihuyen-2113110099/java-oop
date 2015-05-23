b1b0d51df00378b0629b317e932d36eded964e0a
Admin <Admin@D20928>
2015-05-23 14:28:26 +0700

    a

---
+++ example02/src/example02/Rectangle.java
@@ -0,0 +1,16 @@
+package example02;
+
+public class Rectangle {
+	int width;
+	int height;
+	public Rectangle (int a, int b)
+	{
+		width=a;
+		height=b;
+	}
+	public int area(){
+		return(width*height);
+	}
+	}
+
+
---
+++ example02/src/example02/main.java
@@ -0,0 +1,16 @@
+package example02;
+
+import example02.Rectangle;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Rectangle recta=new Rectangle(3,4);
+		Rectangle rectb=new Rectangle(5,6);
+		System.out.println("rect a:"+recta.area());
+		System.out.println("rect b:"+rectb.area());
+			}
+	}
+
+
