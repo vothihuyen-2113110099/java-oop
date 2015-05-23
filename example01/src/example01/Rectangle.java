17f017e9ae3f7e3326cdc6d9f2f262f5b1cedb37
Admin <Admin@D20928>
2015-05-23 14:27:44 +0700

    abc

---
+++ example01/src/example01/Rectangle.java
@@ -0,0 +1,13 @@
+package example01;
+
+public class Rectangle {
+int x, y;
+public void setValues(int a, int b)
+{
+	x=a;
+	y=b;
+}
+public int area(){
+	return(x*y);
+}
+}
---
+++ example01/src/example01/main.java
@@ -0,0 +1,15 @@
+package example01;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Rectangle rect=new Rectangle();
+Rectangle rectb=new Rectangle();
+rect.setValues(3,4);
+rect.setValues(5,6);
+System.out.println("rect a:"+rect.area());
+System.out.println("rect b:"+rectb.area());
+	}
+
+}
